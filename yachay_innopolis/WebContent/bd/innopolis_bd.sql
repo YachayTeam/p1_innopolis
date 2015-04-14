dfc9753a2ad4be4683d21d3fe710f827f766efbe
TikoCisneros <tiko.cisneros@gmail.com>
2015-04-14 14:21:15 -0500

    Full SQL

--- yachay_innopolis/WebContent/bd/innopolis_bd.sql
+++ yachay_innopolis/WebContent/bd/innopolis_bd.sql
@@ -147,4 +147,95 @@ alter table SOLICIDETALLE
    add constraint FK_SOLICIDE_REFERENCE_RECURSO foreign key (ID_RECURSO)
       references RECURSO (ID_RECURSO)
       on delete restrict on update restrict;
+      
+/*==============================================================*/
+/* SECUENCIAS                                           */
+/*==============================================================*/
+      
+      CREATE SEQUENCE public.seq_tipoevento
+   INCREMENT 1
+   START 1;
+   
+   ALTER TABLE tipoevento
+   ALTER COLUMN id_tipo_evento SET DEFAULT nextval('seq_tipoevento');
+   
+   CREATE SEQUENCE public.seq_eventos
+   INCREMENT 1
+   START 1;
 
+   ALTER TABLE eventos
+   ALTER COLUMN id_evento SET DEFAULT nextval('seq_eventos');
+ALTER TABLE eventos
+  DROP CONSTRAINT fk_eventos_reference_solicica;
+ALTER TABLE eventos
+  DROP CONSTRAINT fk_eventos_reference_tipoeven;
+ALTER TABLE eventos
+  ADD CONSTRAINT fk_eventos_reference_solicica FOREIGN KEY (id_solcab)
+      REFERENCES solicicabecera (id_solcab) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE eventos
+  ADD CONSTRAINT fk_eventos_reference_tipoeven FOREIGN KEY (id_tipo_evento)
+      REFERENCES tipoevento (id_tipo_evento) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT;
+      
+CREATE SEQUENCE public.seq_recursotipo
+   INCREMENT 1
+   START 1;
+   
+ALTER TABLE recursotipo
+   ALTER COLUMN id_rectipo SET DEFAULT nextval('seq_recursotipo');
+
+   CREATE SEQUENCE public.seq_recursoestado
+   INCREMENT 1
+   START 1;
+   
+   ALTER TABLE recursoestado
+   ALTER COLUMN id_recest SET DEFAULT nextval('seq_recursoestado');
+   
+   CREATE SEQUENCE public.seq_soliciestado
+   INCREMENT 1
+   START 1;
+   
+   ALTER TABLE soliciestado
+   ALTER COLUMN id_solest SET DEFAULT nextval('seq_soliciestado');
+   
+   CREATE SEQUENCE public.seq_solicicabecera
+   INCREMENT 1
+   START 1;
+   
+   ALTER TABLE solicicabecera
+   ALTER COLUMN id_solcab SET DEFAULT nextval('seq_solicicabecera');
+ALTER TABLE solicicabecera
+  DROP CONSTRAINT fk_solicica_reference_solicies;
+ALTER TABLE solicicabecera
+  ADD CONSTRAINT fk_solicica_reference_solicies FOREIGN KEY (id_solest)
+      REFERENCES soliciestado (id_solest) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT;
+      
+      CREATE SEQUENCE public.seq_solicidetalle
+   INCREMENT 1
+   START 1;
+   
+   ALTER TABLE solicidetalle
+   ALTER COLUMN id_soldet SET DEFAULT nextval('seq_solicidetalle');
+ALTER TABLE solicidetalle
+  DROP CONSTRAINT fk_solicide_reference_recurso;
+ALTER TABLE solicidetalle
+  DROP CONSTRAINT fk_solicide_reference_solicica;
+ALTER TABLE solicidetalle
+  ADD CONSTRAINT fk_solicide_reference_recurso FOREIGN KEY (id_recurso)
+      REFERENCES recurso (id_recurso) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE solicidetalle
+  ADD CONSTRAINT fk_solicide_reference_solicica FOREIGN KEY (id_solcab)
+      REFERENCES solicicabecera (id_solcab) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT;
+   
+      
+      
+
+   
+   
+   
+
+   
