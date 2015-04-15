1378fcfe9b65130d5ee43d3899093a1dd4992761
JuanK <juank20097@gmail.comn>
2015-04-15 12:40:27 -0500

    -correccion managerreservas

--- yachay_innopolis/src/innopolis/manager/ManagerReservas.java
+++ yachay_innopolis/src/innopolis/manager/ManagerReservas.java
@@ -14,6 +14,10 @@ public class ManagerReservas {
 	//Solicitud Temporal
 	private Solicicabecera soliTemp;
 	
+	//Almacenar tipos y estados
+		private static Recursoestado re;
+		private static Recursotipo rt;
+		private static Recursodisponible rd;
 	
 	public ManagerReservas()
 	{
@@ -21,59 +25,107 @@ public class ManagerReservas {
 	}
 	
 	
-// ------RECURSOS-------
+	// ------RECURSOS-------
 	
-// listar todos los recursos 
-	@SuppressWarnings("unchecked")
-	public List<Recurso> findAllRecurso(){
-		return mDAO.findAll(Recurso.class);
-	}
-	
-// listar todos los TipoRecursos 
+	// listar todos los recursos 
 		@SuppressWarnings("unchecked")
-		public List<Recurso> findAllTipoRecurso(){
-			return mDAO.findAll(Recursotipo.class);
+		public List<Recurso> findAllRecurso(){
+			return mDAO.findAll(Recurso.class);
 		}
-	
-//buscar recurso por ID
-	public Recurso RecursoByID(Integer id_recurso) throws Exception{
-		return (Recurso) mDAO.findById(Recurso.class, id_recurso);
-	}
-	
-//insertar los recursos
-	public void insertarRecurso(Integer capacidad, String descripcion, String lugar, String nombre,Recursotipo rt, Recursoestado re) throws Exception{
-		Recurso r = new Recurso();
-		r.setCapacidad(capacidad);
-		r.setDescripcion(descripcion);
-		r.setLugar(lugar);
-		r.setNombre(nombre);
-		r.setRecursotipo(rt);
-		r.setRecursoestado(re);
-		mDAO.insertar(r);
-	}
-	
-//insertar tipo de recurso
-	public void insertarTipoRecurso(String tipo) throws Exception{
-		Recursotipo rt = new Recursotipo();
-		rt.setTipo(tipo);
-		mDAO.insertar(rt);
-	}
-
-//editar los recursos
-	public void editarRecurso(Integer idRecurso, Integer capacidad, String descripcion, String lugar, String nombre, Recursotipo rt){
-		try {
-			Recurso r = this.RecursoByID(idRecurso);
+		
+	// listar todos los TipoRecursos 
+			@SuppressWarnings("unchecked")
+			public List<Recursotipo> findAllTipoRecurso(){
+				return mDAO.findAll(Recursotipo.class);
+			}
+		
+	// listar todos los RecursosEstados 
+					@SuppressWarnings("unchecked")
+					public List<Recursoestado> findAllRecursoEstado(){
+						return mDAO.findAll(Recursoestado.class);
+					}
+	// listar todos los RecursosEstados 
+					@SuppressWarnings("unchecked")
+					public List<Recursoestado> findAllRecursoDisponibles(){
+						return mDAO.findAll(Recursodisponible.class);		
+					}
+					
+	//buscar recurso por ID
+		public Recurso findRecursoByID(Integer id_recurso) throws Exception{
+			return (Recurso) mDAO.findById(Recurso.class, id_recurso);
+		}
+		//buscar RecursoTipo por ID
+			public Recursotipo findRecursoTipoByID(Integer id_rec_tipo) throws Exception{
+				return (Recursotipo) mDAO.findById(Recursotipo.class, id_rec_tipo);
+			}
+			
+			//buscar RecursoEstado por ID
+			public Recursoestado findRecursoEstadoByID(Integer id_rec_estado) throws Exception{
+				return (Recursoestado) mDAO.findById(Recursoestado.class, id_rec_estado);
+			}
+		
+			//buscar RecursoDisponible por ID
+					public Recursodisponible findRecursoDisponibleByID(Long id_rec_disponible) throws Exception{
+						return (Recursodisponible) mDAO.findById(Recursodisponible.class, id_rec_disponible);
+					}
+	//insertar los recursos
+		public void insertarRecurso(Integer capacidad, String descripcion, String lugar, String nombre, String imagen ) throws Exception{
+			Recurso r = new Recurso();
 			r.setCapacidad(capacidad);
 			r.setDescripcion(descripcion);
 			r.setLugar(lugar);
 			r.setNombre(nombre);
+			r.setImagen(imagen);
 			r.setRecursotipo(rt);
-			mDAO.actualizar(r);
-		} catch (Exception e) {
-			System.out.println("Error_mod_recurso");
-			e.printStackTrace();
+			r.setRecursoestado(re);
+			r.setRecursodisponible(rd);
+			mDAO.insertar(r);
 		}
-	}
+		
+	//insertar tipo de recurso
+		public void insertarTipoRecurso(String tipo) throws Exception{
+			Recursotipo rt = new Recursotipo();
+			rt.setTipo(tipo);
+			mDAO.insertar(rt);
+		}
+
+	//editar los recursos
+		public void editarRecurso(Integer idRecurso, Integer capacidad, String descripcion, String lugar, String nombre){
+			try {
+				Recurso r = this.findRecursoByID(idRecurso);
+				r.setCapacidad(capacidad);
+				r.setDescripcion(descripcion);
+				r.setLugar(lugar);
+				r.setNombre(nombre);
+				r.setRecursotipo(rt);
+				mDAO.actualizar(r);
+			} catch (Exception e) {
+				System.out.println("Error_mod_recurso");
+				e.printStackTrace();
+			}
+		}
+		
+		//metodo para asignar el RecursoTipo al Recurso
+	 	public Recursotipo asignarRecursoTipo(Integer idRecTipo) {
+	 		try {
+				rt=findRecursoTipoByID(idRecTipo);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	 		return rt;
+		}
+	 	
+	 	//metodo para asignar el RecursoEstado al Recurso
+	 	 	public Recursoestado asignarRecursoEstado(Integer idRecEstado) {
+	 	 		try {
+					re=findRecursoEstadoByID(idRecEstado);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	 	 		return re;
+	 		}
 
 	// ------SOLICITUDES-------
 	
@@ -116,7 +168,7 @@ public class ManagerReservas {
 					//FALTA VALIDACIONES DE RECURSOS por fecha y capacidad
 					
 					//Busqueda Recurso Libre--- Cambiar Método
-					rec = this.RecursoByID(id_recurso);
+					rec = this.findRecursoByID(id_recurso);
 					//Crear detalle
 					det = new Solicidetalle();
 					det.setRecurso(rec);
