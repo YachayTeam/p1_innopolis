ab03f7ecf8cca2dbab2082003a74fffd48667667
JuanK <juank20097@gmail.comn>
2015-04-16 14:40:51 -0500

    -adicion de datos.sql

--- yachay_innopolis/WebContent/bd/datos_bd.sql
+++ yachay_innopolis/WebContent/bd/datos_bd.sql
@@ -9,3 +9,19 @@ insert into recursodisponible values (default, 'Desactivado');
 insert into recurso values (default, 3, 1, 'Fab-Lab', 20, 'Descripcion fab-lab', 'Yachay',null);
 insert into recurso values (default, 3, 1, 'Sala Samsung', 10, 'Descripcion samsung', 'Yachay',null);
 insert into recurso values (default, 3, 1, 'Cafetería', 15, 'Descripcion cafetería', 'Yachay',null);
+
+/**********SOLICITUD**********/
+
+INSERT INTO soliciestado VALUES (1, 'pendiente');
+INSERT INTO soliciestado VALUES (2, 'finalizado');
+INSERT INTO soliciestado VALUES (3, 'aprobado');
+INSERT INTO soliciestado VALUES (4, 'negado');
+
+INSERT INTO solicicabecera VALUES (1, 1, 'ojon', 'ojos', '2015-07-07', '12:00:00', '15:00:00');
+INSERT INTO solicicabecera VALUES (2, 1, 'ojon', 'ojos', '2015-07-07', '12:00:00', '15:00:00');
+INSERT INTO solicicabecera VALUES (3, 1, 'ojon', 'ojos', '2015-07-07', '12:00:00', '15:00:00');
+
+INSERT INTO solicidetalle VALUES (1, 1, 1, 20);
+INSERT INTO solicidetalle VALUES (2, 2, 2, 200);
+INSERT INTO solicidetalle VALUES (3, 3, 3, 2000);
+INSERT INTO solicidetalle VALUES (4, 1, 2, 20000);
