43ae3fb8e5af6256029be4251358582ff91c9f91
lcisneros <lcisneros@MXL2030BSG.yachay.ep>
2015-04-30 14:20:43 -0500

    Validar Horas

--- yachay_innopolis/src/innopolis/controller/SolicitudBean.java
+++ yachay_innopolis/src/innopolis/controller/SolicitudBean.java
@@ -211,14 +211,19 @@ public class SolicitudBean {
 			//Modificacion de Horas
 			setHorainicio(this.fechaAtiempo(getH_inicio()));
 			setHorafin(this.fechaAtiempo(getH_fin()));
-			//SolicitudTemporal
-			solicitudCabTem = manager.crearSolicitudTmp(getDireccion(), getActividad(), getObjetivo(), getJustificacion(), getFecha(), getHorafin(), getHorainicio()); 
-			id_recurso=0; 
-			capacidad_recurso=0;
-			solicitudCabTmpGuardada=false;
-			//Cargar Listado----
-			select = this.getlistaRecursosLibres();
-			resp="soldet";
+			if(getHorafin().getTime()>=getHorainicio().getTime()){
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Verifique su horario de solicitud.", null));
+			}else{
+				//SolicitudTemporal
+				solicitudCabTem = manager.crearSolicitudTmp(getDireccion(), getActividad(), getObjetivo(), getJustificacion(), getFecha(), getHorafin(), getHorainicio()); 
+				id_recurso=0; 
+				capacidad_recurso=0;
+				solicitudCabTmpGuardada=false;
+				//Cargar Listado----
+				select = this.getlistaRecursosLibres();
+				resp="soldet";
+			}
+			
 		} catch (Exception e) {
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error al crear la solicitud.", null));
 		}
