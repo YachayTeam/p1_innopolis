0817d76f3b0c4a228c4367eaaa9fb2ec6b7a02bb
lcisneros <lcisneros@MXL2030BSG.yachay.ep>
2015-05-06 15:06:57 -0500

    Clase ejemplo en paquete TEST para la lectura de servicio REST

---
+++ yachay_innopolis/src/innopolis/test/GetREST.java
@@ -0,0 +1,69 @@
+package innopolis.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+public class GetREST {
+	public static void main(String[] args) {
+		 
+		String url = "http://10.2.2.80/prueba/endpoint_services/user.json";
+		  
+		try {
+			String getJSON = getRest(url);
+			System.out.println(getJSON);
+			//LEER JSON
+			System.out.println("--------------------------------------------------LEER---------------------------------------------------");
+			JSONArray array=(JSONArray) JSONValue.parse(getJSON);
+			//SIZE -1
+			System.out.println("Tamaño:"+array.size());
+			//OBJETO DEL ARRAY
+			JSONObject obj=(JSONObject)array.get(2);
+			System.out.println(obj.get("name"));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	 
+	}
+	
+	public static String getRest(String url) throws Exception{
+		String resp = "";
+		
+		try {
+			 
+			URL url_serv = new URL(url);
+			HttpURLConnection conn = (HttpURLConnection) url_serv.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+	 
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed: HTTP error: "+ conn.getResponseCode());
+			}
+	 
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+				(conn.getInputStream())));
+	 
+			String output;
+			while ((output = br.readLine()) != null) {
+				resp += output;
+			}
+	 
+			conn.disconnect();
+	 
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("MalformedURLException: "+ e.getMessage());
+		} catch (IOException e) {
+			throw new RuntimeException("IOException: "+ e.getMessage());
+		}
+		
+		return resp;
+	}
+}
